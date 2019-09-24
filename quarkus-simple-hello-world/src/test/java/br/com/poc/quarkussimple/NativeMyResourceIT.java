18faa92dd0514eba59fa94fe95a47bb1aa30b6fc
gustavo <gustavo.lima@ilegra.com>
2019-09-24 15:27:13 -0300

    quarkus simple hello world

---
+++ quarkus-simple-hello-world/src/test/java/br/com/poc/quarkussimple/NativeMyResourceIT.java
@@ -0,0 +1,9 @@
+package br.com.poc.quarkussimple;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeMyResourceIT extends MyResourceTest {
+
+    // Execute the same tests but in native mode.
+}
