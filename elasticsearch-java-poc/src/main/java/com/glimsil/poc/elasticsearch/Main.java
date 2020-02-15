3a802c92c1246772057e6389f9c83b64d32ac331
Gustavo <gusdlim@gmail.com>
2020-02-15 19:59:58 -0300

    removing unused variables and imports

--- elasticsearch-java-poc/src/main/java/com/glimsil/poc/elasticsearch/Main.java
+++ elasticsearch-java-poc/src/main/java/com/glimsil/poc/elasticsearch/Main.java
@@ -5,17 +5,13 @@ import com.glimsil.poc.elasticsearch.model.Document;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Random;
 import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         ElasticSearchClientProvider elasticSearchClientProvider = new ElasticSearchClientProvider();
-        Random random = new Random();
         System.out.println("Insertion started.");
         for (int i = 0; i < 1000; i++) {
-            String documentUri = i + "-" + UUID.randomUUID().toString() + ".com.br";
-            String basis = random.nextInt(30) + "/" + random.nextInt(12);
             Document document = new Document();
             document.setDocumentLink(i + "-" + UUID.randomUUID().toString() + ".com.br");
             document.setDocumentName("Name " + i);
